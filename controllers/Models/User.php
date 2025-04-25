15ec35c9bb28ee69ffa5c85d2e4c7161261d38f4
BlearyRat381 <37387115+BlearyRat381@users.noreply.github.com>
2025-04-25 17:38:46 -0300

    Update Models

---
+++ controllers/Models/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $table = 'usuarios';
+    protected $primaryKey = 'id_usuario';
+    
+    protected $fillable = [
+        'nome',
+        'email',
+        'data_nascimento',
+        'id_grupo',
+    ];
+
+    public function grupo()
+    {
+        return $this->belongsTo(Grupo::class, 'id_grupo', 'id_grupo');
+    }
+
+}
